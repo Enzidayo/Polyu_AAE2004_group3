5616bceee90488830121d3433d46555849408fbf
QUECIE <q.u.c.i@outlook.com>
2022-10-25 14:51:06 +0800

    a little test to find where is python installed

---
+++ testforfolder.py
@@ -0,0 +1,4 @@
+import os
+path = "C:/Users"
+path = os.path.realpath("C:\Users\user\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Python 3.10")
+os.startfile("C:\Users\user\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Python 3.10")
